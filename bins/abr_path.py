dcbc33ec25cd14344b34e0e631e2f0ae3c588367
Quinten Palmer <quintenpalmer@protonmail.com>
2024-01-30 16:58:51 -0500

    Introduce `abr_path.py`
    This prints a pretty value for a given path.
    
    I wrote this python so long ago, but it still looks reasonable and still
    works for me.

---
+++ bins/abr_path.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import sys
+import re
+import os
+
+
+def shorten_single(directory):
+    return directory[0] if directory else directory
+
+
+def shorted_leading(directory_list):
+    return list(map(shorten_single, directory_list[:-1])) + [directory_list[-1]]
+
+
+def shorten_path(full_path):
+    return '/'.join(shorted_leading(full_path.split('/')))
+
+
+def pretty_path(full_path):
+    full_path = re.sub('/home/\w+', '~', full_path)
+    return shorten_path(full_path)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        path = os.getcwd()
+    else:
+        path = sys.argv[1]
+    print(pretty_path(path))
