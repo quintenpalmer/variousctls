c67fcb48cb5d6f4e3648810dd170f3f1b9d37687
Quinten Palmer <quintenpalmer@protonmail.com>
2024-01-31 00:30:35 -0500

    Still cp the file, even if it doesn't exist in ~/.bin

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,9 @@ for file in $(ls bins/*); do
         fi
     else
         echo "  <no target file>"
+        # We still use the -i
+        # just in case a file has snuck in
+        # since we asked if it existed above
+        cp -i ${file} ~/.bin
     fi
 done
