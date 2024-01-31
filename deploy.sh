389182843014b5e5c050a2d1c5235b118c729a58
Quinten Palmer <quintenpalmer@protonmail.com>
2024-01-31 00:28:27 -0500

    Only cp file interactively if there is a diff

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ for file in $(ls bins/*); do
         diff ${file} ~/.bin/$(basename ${file})
         if [ $? -eq 0 ]; then
             echo "  <no diff>"
+        else
+            cp -i ${file} ~/.bin/
         fi
     else
         echo "  <no target file>"
     fi
-
-    cp -i ${file} ~/.bin/
 done
