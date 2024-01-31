e0f89d83ba3e9050952985a92103b12fa05b8a89
Quinten Palmer <quintenpalmer@protonmail.com>
2024-01-31 13:54:40 -0500

    Prefer [color]diff -u option

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ for file in $(ls bins/*); do
     TARGET_FILE=~/.bin/$(basename ${file})
     echo "${TARGET_FILE}:"
     if [ -f "${TARGET_FILE}" ]; then
-        colordiff ~/.bin/$(basename ${file}) ${file}
+        colordiff -u ~/.bin/$(basename ${file}) ${file}
         if [ $? -eq 0 ]; then
             echo "  <no diff>"
         else
