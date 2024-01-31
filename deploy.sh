50bdb10f0e20e0a547461e4db5fc05bcd7583c4f
Quinten Palmer <quintenpalmer@protonmail.com>
2024-01-31 03:12:55 -0500

    Prefer colordiff in `deploy.sh` diffing

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ for file in $(ls bins/*); do
     TARGET_FILE=~/.bin/$(basename ${file})
     echo "${TARGET_FILE}:"
     if [ -f "${TARGET_FILE}" ]; then
-        diff ${file} ~/.bin/$(basename ${file})
+        colordiff ${file} ~/.bin/$(basename ${file})
         if [ $? -eq 0 ]; then
             echo "  <no diff>"
         else
