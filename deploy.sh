bd92af18454fccd0f3a7a84aab4b01373f093f67
Quinten Palmer <quintenpalmer@protonmail.com>
2024-01-31 00:27:19 -0500

    Prettier output for deploy.sh
    This changes how <no diff> and <no output file> display and adds a `:`
    after the filename so that it's more clear for which file the <output>
    relates to.

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 #
 for file in $(ls bins/*); do
     TARGET_FILE=~/.bin/$(basename ${file})
-    echo ${TARGET_FILE}
+    echo "${TARGET_FILE}:"
     if [ -f "${TARGET_FILE}" ]; then
         diff ${file} ~/.bin/$(basename ${file})
         if [ $? -eq 0 ]; then
-            echo "<no diff>"
+            echo "  <no diff>"
         fi
     else
-        echo "<no target file>"
+        echo "  <no target file>"
     fi
 
     cp -i ${file} ~/.bin/
