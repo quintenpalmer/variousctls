47dd4ce99254e96a716f0aeee631552de2f2a4bd
Quinten Palmer <quintenpalmer@protonmail.com>
2024-01-31 13:59:13 -0500

    Also prefer [color]diff -w
    To avoid whitespace changes; generally makes the diff easier to see.

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ for file in $(ls bins/*); do
     TARGET_FILE=~/.bin/$(basename ${file})
     echo "${TARGET_FILE}:"
     if [ -f "${TARGET_FILE}" ]; then
-        colordiff -u ~/.bin/$(basename ${file}) ${file}
+        colordiff -w -u ~/.bin/$(basename ${file}) ${file}
         if [ $? -eq 0 ]; then
             echo "  <no diff>"
         else
