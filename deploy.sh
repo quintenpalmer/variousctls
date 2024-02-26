e1e8574ee026ecb245cdab7ddda17998ef1b780f
Quinten Palmer <quintenpalmer@protonmail.com>
2024-02-26 11:07:49 -0500

    Wrap main logic in `main` function
    As we're about to start using functions, we need this in a main to make
    bash happy (if we want to put the functions in arbitrary orders, that
    is [which we do]).

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-for file in $(ls bins/*); do
-    TARGET_FILE=~/.bin/$(basename ${file})
-    echo "${TARGET_FILE}:"
-    if [ -f "${TARGET_FILE}" ]; then
-        colordiff -w -u ~/.bin/$(basename ${file}) ${file}
-        if [ $? -eq 0 ]; then
-            echo -e "  \033[1;31m<no diff>\033[0;0m"
+function main(){
+    for file in $(ls bins/*); do
+        TARGET_FILE=~/.bin/$(basename ${file})
+        echo "${TARGET_FILE}:"
+        if [ -f "${TARGET_FILE}" ]; then
+            colordiff -w -u ~/.bin/$(basename ${file}) ${file}
+            if [ $? -eq 0 ]; then
+                echo -e "  \033[1;31m<no diff>\033[0;0m"
+            else
+                cp -i ${file} ~/.bin/
+            fi
         else
-            cp -i ${file} ~/.bin/
+            echo "  <no target file>"
+            # We still use the -i
+            # just in case a file has snuck in
+            # since we asked if it existed above
+            cp -i ${file} ~/.bin
         fi
-    else
-        echo "  <no target file>"
-        # We still use the -i
-        # just in case a file has snuck in
-        # since we asked if it existed above
-        cp -i ${file} ~/.bin
-    fi
-done
+    done
+}
+
+main $@
