58c1bffd28268e8939236c77e29481c9568ee73c
Quinten Palmer <quintenpalmer@protonmail.com>
2025-03-20 13:21:13 -0400

    Fix `shuffle.py`
    We don't actually use this anymore, as `sort -R` does the same thing and
    is definitely faster.

--- bins/shuffle.py
+++ bins/shuffle.py
@@ -9,7 +9,7 @@ def main():
         values.append(current_line.rstrip('\n'))
 
     shuffled_values = []
-    for max_existing in range(len(values)-1, 0, -1):
+    for max_existing in range(len(values)-1, -1, -1):
         index = random.randint(0, max_existing)
         plucked_value = values.pop(index)
         shuffled_values.append(plucked_value)
